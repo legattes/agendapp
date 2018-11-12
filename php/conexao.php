35ccad464e0dba6444f579a9dbc06b81e36f46fb
Lucas arantes <lucas.arantes@onset.com.br>
2018-11-12 14:31:06 -0200

    novo host

--- php/conexao.php
+++ php/conexao.php
@@ -7,13 +7,11 @@
 
 class Conexao{
     public function connect(){    
-        $server = 'fdb22.awardspace.net';
-        $user = '2874449_agenda';
+        $server = 'localhost';
+        $user = 'u112785488_zion';
         $pass = '3Uj7zy98@';
-        $db = '2874449_agenda';
+        $db = 'u112785488_agapp';
 
-        return $conn = mysqli_connect($server, $user, $pass, $db);
+        return mysqli_connect($server, $user, $pass, $db);
     }
 }
-
-?>
